2841ab68a23e0923546ec6a5b7fcfee3a7167403
GreyhatDemonlord <brianhansen.work@gmail.com>
2022-12-30 10:17:10 +0100

    Added flash for log in

--- app/controllers/main_controller.rb
+++ app/controllers/main_controller.rb
@@ -1,3 +1,6 @@
 class MainController < ApplicationController
-  def index; end
+  def index
+    flash.now[:notice] = 'Logged in succesfully 🚀'
+    flash.now[:alert] = 'Password or email address is incorrect.'
+  end
 end
